34b3bb840e353a181598613bdb5352a6af38d934
trsv.egor <eg0r-taras0v@yandex.ru>
2024-05-07 14:06:50 +0300

    First commit (v1)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Создаём объект калькулятор
+        Calc calc = new Calc(Input.getPersonCount());
+        //Запрос у пользователя товаров
+        Input.getAndAddPositions(calc);
+        //Показать результат
+        Output.showAndCalculate(calc);
     }
 }
---
+++ src/main/java/Product.java
@@ -0,0 +1,11 @@
+//Используется для хранения позиции товара с ценой
+public class Product {
+    String product;
+    Double price;
+
+    public Product(String product, Double price){
+        this.product = product;
+        this.price = price;
+    }
+
+}
